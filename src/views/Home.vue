1db9858fe2e360b297e292b431ae0b714656c783
nabildz <nabilkechroud@gmail.com>
2021-06-02 18:28:47 +0200

    Added movies api in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,95 @@
 <template>
-    <!--
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ]
-  }
-  ```
--->
-<div class="">
-  <div class="mx-auto py-4 px-4 max-w-7xl lg:py-6">
-    <div class="space-y-12">
-      <h2 class="text-xl font-medium tracking-tight sm:text-3xl">New Movies</h2>
+  <div class="">
+    <div class="mx-auto py-4 px-4 max-w-7xl lg:py-6">
+      <div class="space-y-12">
+        <h2 class="text-xl font-medium tracking-tight sm:text-3xl">
+          New Movies
+        </h2>
 
-      <ul class="space-y-12 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 lg:gap-y-12 lg:space-y-0">
-        <li>
-          <div class="bg-white shadow rounded-xl p-6 space-y-2 sm:grid sm:grid-cols-3 sm:gap-6 sm:space-y-0 lg:gap-8">
-            <div class="h-0 aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
-              <img class="object-cover shadow-lg rounded-lg" src="https://image.tmdb.org/t/p/w500//6wxfWZxQcuv2QgxIQKj0eYTdKTv.jpg" alt="">
-            </div>
-            <div class="sm:col-span-2">
-              <div class="space-y-4">
-                <div class="text-lg sm:text-2xl leading-6 font-medium space-y-2 sm:space-y-1">
-                  <h3>The Unholy</h3>
-                  <p class="font-bold text-xl">
-                      <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-base font-medium bg-green-100 text-green-800">
-  8.2
-</span></p>
-                </div>
-                <div class="text-xs">
-                  <p class="text-gray-500 justfiy">Alice, a young hearing-impaired girl who, after a supposed visitation from the Virgin Mary, is inexplicably able to hear, speak and heal the sick. As word spreads and people from near and far flock to witness her miracles, a disgraced journalist hoping to revive his career visits the small New England town to investigate. When terrifying events begin to happen all around, he starts to question if these phenomena are the works of the Virgin Mary or something much more sinister.</p>
+        <ul
+          class="space-y-12 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-6 lg:gap-y-10 lg:space-y-0"
+        >
+          <li v-for="movie in movies" :key="movie.key" class="transform hover:scale-95 transition-transform duration-1000 ease-out">
+            <div
+              class="bg-white shadow rounded-xl p-6 space-y-2 grid grid-cols-3 gap-3 sm:grid sm:grid-cols-3 sm:gap-6 sm:space-y-0 lg:gap-8 "
+            >
+              <div
+                class="h-0 aspect-w-3 aspect-h-4 sm:aspect-w-3 sm:aspect-h-4  mr-2"
+              >
+                <img
+                  class="object-cover shadow-lg rounded-lg transform hover:scale-125 transition-transform duration-1000 ease-out "
+                  :src="'https://image.tmdb.org/t/p/w500/'+movie.poster_path"
+                  alt=""
+                />
+              </div>
+              <div class="mt-3 sm:mt-0 col-span-2">
+                <div class="space-y-4">
+                  <div
+                    class="text-lg sm:text-2xl leading-6 font-medium space-y-2 sm:space-y-1"
+                  >
+                    <h3>{{ movie.title }}</h3>
+                    <p class="font-bold text-xl">
+                      <span
+                        class="inline-flex items-center px-2.5 py-0.5 rounded-md text-base font-medium 
+                        "
+
+                          :class="{
+                      'bg-red-100 text-red-800': movie.vote_average <= 2.5,
+                      'bg-yellow-100 text-yellow-800': movie.vote_average > 5.0 && movie.vote_average < 7.5,
+                      'bg-green-100 text-green-800': movie.vote_average >= 7.5 && movie.vote_average <= 10,
+                    }"
+                      >
+                        {{ movie.vote_average }}
+                      </span>
+                    </p>
+                  </div>
+                  <div class="text-xs">
+                    <p class="text-gray-500 max-h-12 truncate">
+                        {{ movie.overview }}
+                    </p>
+                  </div>
+
+                
                 </div>
               </div>
             </div>
-          </div>
-        </li>
-
-        <!-- More people... -->
-      </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
-</div>
-
 </template>
+
+
+<script>
+// import { debounce } from "lodash";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      isLoading: false,
+      searchQuery: null,
+      movies: [],
+    };
+  },
+  beforeMount() {
+    this.getMovies();
+  },
+  methods: {
+    async getMovies() {
+      this.isLoading = !this.isLoading;
+      fetch("http://movie-app-backend.test/api/movies", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => (this.movies = data.data))
+        .then((data) => (this.isLoading = !this.isLoading));
+    },
+  },
+};
+</script>
